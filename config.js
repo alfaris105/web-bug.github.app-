6ad22b03590b61f3e4d12bbd25e7c43986fba196
alfaris105 <raafialfaris7@gmail.com>
2025-10-21 17:23:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "7971018631:AAE5ZRUxT3am1Lo89VAeaRRiEHR61kb3_eo",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7901712927", // Ubah Jadi Id Mu !!!
+  port: "2002", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.226.186" // Ubah Jadi Ip Vps Mu !!!
+};
